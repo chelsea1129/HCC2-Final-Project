52942346cf259c7f4fcb15ef61fcc79ed44a0911
Qiaosi Wang <wqs@uw.edu>
2018-11-19 13:09:03 -0500

    tried installing pandas but failed

--- MergingFiles.py
+++ MergingFiles.py
@@ -1,6 +1,8 @@
 from tkinter import *
 from tkinter.filedialog import askopenfilenames
 from Step import Step
+import pandas as pd
+
 import ntpath
 
 class MergeFiles(Step):
@@ -8,6 +10,8 @@ class MergeFiles(Step):
         super().__init__(parent, data, stepname)
 
         self.col_list = []
+        self.file_list = []
+        self.data_frame = []
 
         self.select_files = Label(self, text='Please select the CSV files you want to merge '
                                              '(press shift to select multiple lines):', wraplength=400)
@@ -16,10 +20,17 @@ class MergeFiles(Step):
         self.select_files_button = Button(self, text='Select', command=self.open_files)
         self.select_files_button.pack()
 
+        self.create_merged_data_frame()
+
     def open_files(self):
         filez = askopenfilenames(parent=self, title='Choose a file')
         file_dir_list = list(filez)
         for dir in file_dir_list:
+            self.file_list.append(ntpath.basename(dir))
             name_string = ntpath.basename(dir)
             self.col_list.append(name_string[:-4])
+
+    def create_merged_data_frame(self):
+        self.data_frame = [pd.read_csv(f) for f in self.file_list]
+        print(self.data_frame)
 
